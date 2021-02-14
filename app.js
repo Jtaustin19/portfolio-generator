4e838d29c75a1272a755fba7ce37fc52f5727d01
Josh <jtaustin19@icloud.com>
2021-02-14 11:15:42 -0600

    generate the a page

--- app.js
+++ app.js
@@ -1,17 +1,7 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const generatePage = require('./src/plate-template.js')
 
 
-const printProfileData = profileDataArr => {
-    // This...
-    for (let i = 0; i < profileDataArr.length; i++) {
-        console.log(profileDataArr[i]);
-    }
 
-    console.log('================');
 
-    // Is the same as this...
-    profileDataArr.forEach((profileItem) => console.log(profileItem));
-};
 
-printProfileData(profileDataArgs);
+module.exports = generatePage;
